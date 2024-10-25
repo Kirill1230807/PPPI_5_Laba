10408fb48b5b46f3e5e9a81337c7882a55cc5122
Bohdan Buryak <buryakbodya@gmail.com>
2024-10-25 10:29:15 +0300

    bohdan

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, my name is Kyrylo!" << endl; // First greeting
-    cout << "Hello, my name is Illya!" << endl;  // Second greeting
+    cout << "Hello, my name is Kyrylo!" << endl;  // First greeting
+    cout << "Hello, my name is Illya!" << endl;   // Second greeting
     cout << "Hello, my name is Yuliia!" << endl;  // third greeting
-    cout << "Hello? my name is Bogdan!" << endl; //fourth greeting
+    cout << "Hello? my name is Bogdan!" << endl;  //fourth greeting
     return 0;
 }
