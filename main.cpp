412348699b151fdc39e25e4aac98958948aa0903
Kirill1230807 <kberegovoj17@gmail.com>
2024-10-31 12:46:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+    cout << "asddasasdasd" << endl;
     cout << "Hello, my name is Kyrylo!" << endl; // First greeting
     cout << "Hello, my name is Illya!" << endl;  // Second greeting
     cout << "Hello, my name is Yuliia!" << endl;  // third greeting
